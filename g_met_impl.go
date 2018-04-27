3823f8e4532c678a0727a45421e06690a5f30626
chaocai <chaocai@192.168.7.71>
2018-04-27 16:46:48 +0800

    Added method to get host address

--- g_met_impl.go
+++ g_met_impl.go
@@ -1,5 +1,14 @@
 package g_met
 
+import (
+	"net"
+)
+
+const (
+	HOST_ADDR     = "HostAddr"
+	MISSING_VALUE = "N/A"
+)
+
 type GMetInstance struct {
 	metWriter    MetWriter
 	metFormatter MetFormatter
@@ -25,4 +34,23 @@ func (gmet *GMetInstance) Flush() {
 
 func (gmet *GMetInstance) Close() error {
 	return gmet.metWriter.Close()
+}
+
+func IpAddress() (MetricItem, error) {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return MetricItem{HOST_ADDR, MISSING_VALUE}, err
+	}
+
+	for _, address := range addrs {
+		// Check if it is ip circle
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return MetricItem{HOST_ADDR, ipnet.IP.String()}, nil
+			}
+
+		}
+	}
+	return MetricItem{HOST_ADDR, MISSING_VALUE}, err
 }
